3929fb8665a3e6c2249883a6fd85bd209d3fc16f
Daniel Golden <dgolden1@bu.edu>
2022-06-23 17:57:52 -0400

    Old version wrong
    Previous version was generating incorrect FASTA file paths

--- FASTA_acquisition_script.R
+++ FASTA_acquisition_script.R
@@ -4,14 +4,21 @@ library(readr)
 
 genbank_assembly <- read.csv("/projectnb2/microbiome/dgolden/Struo2/genomes_for_file_paths/assembly_summary_genbank.csv", header = TRUE)
 mod_vec <- genbank_assembly$ftp_path
-genbank_assembly$fasta_file_paths <- mod_vec
-genbank_assembly$fasta_file_paths <- paste0(genbank_assembly$fasta_file_paths, "_genomic.fna.gz")
+genome_names <- substr(mod_vec, start=58, stop=95)
+genome_names <- paste0(genome_names, "_genomic.fna.gz")
+genome_names <- paste0("/", genome_names)
+full_paths <- paste0(mod_vec, genome_names)
+genbank_assembly$fasta_file_paths <- full_paths
 refseq_assembly <- read.csv("/projectnb2/microbiome/dgolden/Struo2/genomes_for_file_paths/assembly_summary_refseq.csv", header = TRUE)
 mod_vecRS <- refseq_assembly$ftp_path
-refseq_assembly$fasta_file_paths <- mod_vecRS
-refseq_assembly$fasta_file_paths <- paste0(refseq_assembly$fasta_file_paths, "_genomic.fna.gz")
-#write_tsv(genbank_assembly, "genbank_with_filepaths.tsv")
-#write_tsv(refseq_assembly, "refseq_with_filepaths.tsv")
+genome_namesRS <- substr(mod_vecRS, start=58, stop=95)
+genome_namesRS <- paste0(genome_namesRS, "_genomic.fna.gz")
+genome_namesRS <- paste0("/", genome_namesRS)
+full_pathsRS <- paste0(mod_vecRS, genome_namesRS)
+refseq_assembly$fasta_file_paths <- full_pathsRS
+
+#write.csv(genbank_assembly, "/projectnb2/microbiome/dgolden/Struo2/genomes_for_file_paths/genbank_with_filepaths.csv")
+#write.csv(refseq_assembly, "/projectnb2/microbiome/dgolden/Struo2/genomes_for_file_paths/refseq_with_filepaths.csv")
 gb_accession <- genbank_assembly$X..assembly_accession
 rs_accession <- refseq_assembly$X..assembly_accession
 gbrs_accession <- c(gb_accession, rs_accession)
@@ -21,26 +28,13 @@ full_accession <- full_tsv$accession
 full_accession_trim <- substr(full_accession, start=4, stop=18)
 overlap <- intersect(gbrs_accession, full_accession_trim)
 full_tsv$accession_trim <- full_accession_trim
-filtered_GTDB_with_FASTA <- dplyr::filter(full_tsv, full_tsv$accession_trim %in% overlap)
-#write_tsv(filtered_GTDB_with_FASTA, "filt_GTDB_FASTA.tsv")
-
-JGI_org_df <- read.csv("/projectnb2/microbiome/dgolden/Struo2/genomes_for_file_paths/MAG_organisms.csv")
-JGI_proj_df <- read.csv("/projectnb2/microbiome/dgolden/Struo2/genomes_for_file_paths/MAG_projects.csv")
-org_ids_names <- JGI_org_df$Organism.Organism.Name
-proj_ids_names <- JGI_proj_df$Analysis.Project.Analysis.Project.Name
-names(JGI_org_df)[names(JGI_org_df)=="Organism.Organism.Name"] <- "samples_col"
-names(JGI_proj_df)[names(JGI_proj_df)=="Analysis.Project.Analysis.Project.Name"] <- "samples_col"
-#print(org_ids_names == proj_ids_names)
-  # this confirms that the organism and project names are the same
-JGI_org_proj <- merge(JGI_org_df, JGI_proj_df, by="samples_col")
-  # join on same names, similar to SQL joins
-#write_tsv(JGI_org_proj, "JGI_data.tsv")
-JGI_accession <- JGI_org_proj$Analysis.Project.Assembly.Accession
-JGI_overlap <- intersect(gbrs_accession, JGI_accession)
-JGI_overlap_df <- dplyr::filter(JGI_org_proj, Analysis.Project.Assembly.Accession %in% JGI_overlap)
-gbrs_JGI_overlap_df <- dplyr::filter(gbrs_df, X..assembly_accession %in% JGI_overlap)
-names(JGI_org_proj)[names(JGI_org_proj)=="Analysis.Project.Assembly.Accession"] <- "accession_col"
-names(gbrs_JGI_overlap_df)[names(gbrs_JGI_overlap_df)=="X..assembly_accession"] <- "accession_col"
-JGI_final <- merge(JGI_org_proj, gbrs_JGI_overlap_df, by="accession_col")
-names(JGI_final)[names(JGI_final)=="Organism.NCBI.Taxonomy.ID"] <- "taxID_col"
-names(JGI_final)[names(JGI_final)=="fasta_file_paths"] <- "fasta_file_path_col"
+filtered_GTDB <- dplyr::filter(full_tsv, full_tsv$accession_trim %in% overlap)
+filtered_GTDB_with_FASTA <- merge(filtered_GTDB, gbrs_df, by.x = "accession_trim", by.y = "X..assembly_accession")
+filtered_GTDB_with_FASTA_select <- dplyr::select(filtered_GTDB_with_FASTA, accession, accession_trim, checkm_completeness, checkm_contamination,
+                                                 gtdb_genome_representative, gtdb_taxonomy, mimag_high_quality, ncbi_bioproject,
+                                                 ncbi_genbank_assembly_accession, ncbi_organism_name, ncbi_species_taxid, ncbi_taxid,
+                                                 ncbi_taxonomy, bioproject, biosample, taxid, species_taxid, organism_name, assembly_level,
+                                                 ftp_path, fasta_file_paths)
+#write_tsv(filtered_GTDB_with_FASTA, "/projectnb2/microbiome/dgolden/Struo2/custom_dbs/GTDB_release202/metadata/filt_GTDB_FASTA.tsv")
+#write_tsv(filtered_GTDB_with_FASTA_select, "/projectnb2/microbiome/dgolden/Struo2/custom_dbs/GTDB_release202/metadata/filt_GTDB_FASTA_select.tsv")
+#write_csv(gbrs_df, "/projectnb2/microbiome/dgolden/Struo2/genomes_for_file_paths/genbank_refseq_with_accession.csv")

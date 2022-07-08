bf5e835395a1079d9149a90135f5f453364611c1
Daniel Golden <dgolden1@bu.edu>
2022-07-08 17:56:33 -0400

    Update to 7_8_22 version

--- create_GOLD_download_tsv.r
+++ create_GOLD_download_tsv.r
@@ -3,6 +3,9 @@
 
 library(readxl)
 library(tidyverse)
+library(stringr)
+
+# Zoey section: reads in data frame of JGI download data
 
 gold_data_readme <- readxl::read_xlsx("/projectnb2/talbot-lab-data/zrwerbin/soil_genome_db/goldData.xlsx", sheet = 1)
 gold_data_study <- readxl::read_xlsx("/projectnb2/talbot-lab-data/zrwerbin/soil_genome_db/goldData.xlsx", sheet = 2)
@@ -66,26 +69,112 @@ for (i in 1:nrow(genome_info)){
 # Check why ~1000 are NA
 accession_NAs <- genome_info[is.na(genome_info$NCBI_accession),]
 
+# Read in genbank and refseq data
 gbrs_df_read <- read.csv("/projectnb2/microbiome/dgolden/Struo2/genomes_for_file_paths/genbank_refseq_with_accession.csv")
 gbrs_accession <- gbrs_df_read$X..assembly_accession
-JGI_final_read <- read_tsv("/projectnb2/microbiome/dgolden/Struo2/custom_dbs/JGI_data.tsv")
-JGI_accession <- JGI_final_read$accession_col
-zoey_dan_overlap <- intersect(genome_info$NCBI_accession, JGI_accession)
+
+# Read in Dan's JGI query data for sanity check
+JGI_dan_read <- read_tsv("/projectnb2/microbiome/dgolden/Struo2/custom_dbs/JGI_data.tsv")
+JGI_accession_dan <- JGI_dan_read$accession_col
+zoey_dan_overlap <- intersect(genome_info$NCBI_accession, JGI_accession_dan)
+
+# Filter downloaded JGI data to include only genomes for which we have gbrs accession numbers
+# Also filter gbrs data to include only the genomes that are found in the JGI data
 zoey_gbrs_overlap <- intersect(genome_info$NCBI_accession, gbrs_accession)
 genome_info_gbrs_overlap <- dplyr::filter(genome_info, genome_info$NCBI_accession %in% zoey_gbrs_overlap)
-merge_genome_info <- merge(genome_info_gbrs_overlap, gold_data_organism, by.x = "AP NAME", by.y = "ORGANISM NAME")
+gbrs_filt <- dplyr::filter(gbrs_df_read, gbrs_df_read$X..assembly_accession %in% genome_info_gbrs_overlap$NCBI_accession)
+names(gbrs_filt)[names(gbrs_filt)=="X..assembly_accession"] <- "NCBI_accession"
+
+# everything above this needs to be run at once before running the merges below
+# sometimes you otherwise get an error I don't recognize
+
+# Join selected JGI data with gbrs data on accession number, organism name, etc.
+merge_genome_info <- merge(genome_info_gbrs_overlap, gbrs_filt, by = "NCBI_accession")
+merge_genome_info <- merge(merge_genome_info, gold_data_organism, by.x = "AP NAME", by.y = "ORGANISM NAME")
 merge_genome_info <- merge(merge_genome_info, gbrs_df_read, by.x = "NCBI_accession", by.y="X..assembly_accession")
+
+# Select the fields we want and/or need
 genome_info_select <- dplyr::select(merge_genome_info, NCBI_accession, 'AP NAME', 'AP PROJECT GOLD IDS', 'AP GOLD ID',
-                                    'ORGANISM GOLD ID.x', bioproject, biosample, taxid, species_taxid, organism_name,
-                                    fasta_file_paths, 'ORGANISM NCBI GENUS', 'ORGANISM NCBI SPECIES')
-names(genome_info_select)[names(genome_info_select)=="species_taxid"] <- "taxID_col"
-  # most likely NCBI species taxid
-names(genome_info_select)[names(genome_info_select)=="taxID_col"] <- "ncbi_taxid"
-names(genome_info_select)[names(genome_info_select)=="fasta_file_paths"] <- "fasta_file_path_col"
-names(genome_info_select)[names(genome_info_select)=="NCBI_accession"] <- "accession"
+                                    'ORGANISM GOLD ID.x', 'ORGANISM NCBI TAX ID', taxid.x, species_taxid.x, organism_name.x,
+                                    fasta_file_paths.x, 'ORGANISM NCBI GENUS', 'ORGANISM NCBI SPECIES')
+
+# Change the names for clarity and Struo2 use
+names(genome_info_select)[names(genome_info_select)=="species_taxid.x"] <- "ncbi_species_taxid"
+names(genome_info_select)[names(genome_info_select)=="taxid.x"] <- "ncbi_taxid_nonspecies"
+names(genome_info_select)[names(genome_info_select)=="fasta_file_paths.x"] <- "fasta_file_path_URL"
+names(genome_info_select)[names(genome_info_select)=="NCBI_accession"] <- "partial_accession"
 # Need to construct taxonomy column based on species column
 for (i in 1:nrow(genome_info_select)){
   genome_info_select$'ORGANISM NCBI SPECIES'[i] <- sub(" ", "; ", genome_info_select$'ORGANISM NCBI SPECIES'[i])
 }
-names(genome_info_select)[names(genome_info_select)=="ORGANISM NCBI SPECIES"] <- "ncbi_taxonomy"
+names(genome_info_select)[names(genome_info_select)=="ORGANISM NCBI SPECIES"] <- "ncbi_taxonomy_partial"
+fasta_URLs <- genome_info_select$fasta_file_path_URL
+genome_names_JGI <- substr(fasta_URLs, start=83, stop=121)
+genome_names_JGI <- paste0(genome_names_JGI, "_genomic.fna.gz")
+genome_names_JGI_trunc <- str_extract(string = fasta_URLs, pattern = "(?<=v1/).*(?=.fna.gz)")
+genome_names_JGI_fixed <- paste0(genome_names_JGI_trunc, ".fna.gz")
+working_fasta <- c()
+
+# this will take ~2 hours to run. It downloads the .fna.gz files for all the genomes in JGI meeting our criteria
+  #for (i in 1:length(fasta_URLs)) {
+  #  if (genome_names_trunc_fixed[i] == "NA.fna.gz") next
+  #  tryCatch({
+  #    download.file(fasta_URLs[i], paste0("/projectnb2/microbiome/dgolden/Struo2/data/JGI_genomes/", genome_names_trunc_fixed[i]))
+  #    append(working_fasta, paste0("/projectnb2/microbiome/dgolden/Struo2/data/JGI_genomes/", genome_names_trunc_fixed[i]))
+  #  }, error=function(e){cat("ERROR :",conditionMessage(e), "\n")})
+  #}
+
+  # let's extend accession from "GCA_#####" to "GB_GCA_#####"
+JGI_accession <- genome_info_select$partial_accession
+for (i in 1:length(JGI_accession)) {
+  if (substr(JGI_accession[i], start=1, stop=3) == "GCA") {
+    JGI_accession[i] <- paste0("GB_", JGI_accession[i])
+  }
+  if (substr(JGI_accession[i], start=1, stop=3) == "GCF") {
+    JGI_accession[i] <- paste0("RS_", JGI_accession[i])
+  }
+}
+genome_info_select$full_accession <- JGI_accession
+
+taxonomy_species <- genome_info_select$ncbi_taxonomy_partial
+taxonomy_species <- gsub(";", "", taxonomy_species)
+taxonomy_species <- paste0("START", taxonomy_species)
+taxonomy_species <- paste0(taxonomy_species, "END")
+taxonomy_species <- str_extract(string = taxonomy_species, pattern = "(?<=START).*(?=END)")
+taxonomy_species <- paste0("s__", taxonomy_species)
+taxonomy_genus <- genome_info_select$ncbi_taxonomy_partial
+taxonomy_genus <- paste0("START", taxonomy_genus)
+taxonomy_genus <- str_extract(string = taxonomy_genus, pattern = "(?<=START).*(?=; )")
+taxonomy_genus <- paste0("g__", taxonomy_genus)
+full_tax <- paste(taxonomy_genus, taxonomy_species, sep = ";")
+genome_info_select$ncbi_taxonomy_full <- full_tax
+
+JGI_fasta_filepaths <- paste0("/projectnb2/microbiome/dgolden/Struo2/data/JGI_genomes/", genome_names_JGI_fixed)
+genome_info_select$fasta_file_path <- JGI_fasta_filepaths
+names(genome_info_select)[names(genome_info_select)=="AP NAME"] <- "ncbi_organism_name"
+genome_info_select <- dplyr::filter(genome_info_select, genome_info_select$fasta_file_path != "/projectnb2/microbiome/dgolden/Struo2/data/JGI_genomes/NA.fna.gz")
+organism_names_JGI <- genome_info_select$ncbi_organism_name
+unique_organism_names <- make.unique(organism_names_JGI)
+genome_info_select$unique_ncbi_organism_name <- unique_organism_names
 #write_tsv(genome_info_select, "/projectnb2/microbiome/dgolden/Struo2/custom_dbs/JGI_downloads_data_corrected.tsv")
+
+genome_info_distinct_name <- distinct(genome_info_select, ncbi_organism_name, .keep_all = TRUE)
+genome_info_distinct_name_acc <- distinct(genome_info_distinct_name, full_accession, .keep_all = TRUE)
+genome_info_distinct_accession <- distinct(genome_info_select, full_accession, .keep_all = TRUE)
+genome_info_distinct_id <- distinct(genome_info_select, ncbi_species_taxid, .keep_all = TRUE)
+
+unique_names_and_accession <- make.unique(genome_info_distinct_accession$ncbi_organism_name)
+genome_info_distinct_accession$unique_ncbi_organism_name <- unique_names_and_accession
+
+#write_tsv(genome_info_distinct_accession, "/projectnb2/microbiome/dgolden/Struo2/custom_dbs/JGI_downloads_data_distinct_accession_unique_names.tsv")
+#write_tsv(genome_info_distinct_name, "/projectnb2/microbiome/dgolden/Struo2/custom_dbs/JGI_downloads_data_distinct_name.tsv")
+
+premade_tsv <- read_tsv("/projectnb2/microbiome/dgolden/Struo2/custom_dbs/GTDB_release202/taxdump/taxdump/taxID_info.tsv")
+premade_accession <- premade_tsv$name
+JGI_accession_distinct_name <- genome_info_distinct_name_acc$full_accession
+JGI_premade_overlap <- intersect(JGI_accession_distinct_name, premade_accession)
+JGI_accession_filtered <- JGI_accession_distinct_name[! JGI_accession_distinct_name %in% JGI_premade_overlap]
+
+genome_info_distinct_accession_premade_filter <- dplyr::filter(genome_info_distinct_accession, full_accession %in% JGI_accession_filtered)
+#write_tsv(genome_info_distinct_accession, "/projectnb2/microbiome/dgolden/Struo2/custom_dbs/JGI_downloads_data_distinct_name_acc_premade_filter.tsv")
+
